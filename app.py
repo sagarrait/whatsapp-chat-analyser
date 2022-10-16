498d06472673c3395f0225bd17b691edae7f535a
sagarrait <115926304+sagarrait@users.noreply.github.com>
2022-10-16 17:11:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import preprocessor,helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
---
+++ helper.py
@@ -0,0 +1,102 @@
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji
+
+extract = URLExtract()
+
+def fetch_stats(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    # fetch the number of messages
+    num_messages = df.shape[0]
+
+    # fetch the total number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+
+    # fetch number of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+
+    # fetch number of links shared
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+
+    return num_messages,len(words),num_media_messages,len(links)
+
+# most busy users
+#create wordcloud
+#daily_timeline
+#week_activity_map
+#month_activity_map
+#activity_heatmap
+
+
+def most_common_words(selected_user,df):
+
+    f = open('stop_hinglish.txt','r')
+    stop_words = f.read()
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    words = []
+
+    for message in temp['message']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    return most_common_df
+
+def emoji_helper(selected_user,df):
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    emojis = []
+    for message in df['message']:
+        emojis.extend([c for c in message if c in emoji.UNICODE_EMOJI['en']])
+
+    emoji_df = pd.DataFrame(Counter(emojis).most_common(len(Counter(emojis))))
+
+    return emoji_df
+
+def monthly_timeline(selected_user,df):
+
+    if selected_user != 'Overall':
+        df = df[df['user'] == selected_user]
+
+    timeline = df.groupby(['year', 'month_num', 'month']).count()['message'].reset_index()
+
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+
+    timeline['time'] = time
+
+    return timeline
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
